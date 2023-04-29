317381ad546339c6c465b0bfd59d8b3213ef607f
Hizkia Syaukani <hizkiapro980@gmail.com>
2023-04-29 21:38:26 +0700

    Update index Approve

--- application/views/mitrabestari/index.php
+++ application/views/mitrabestari/index.php
@@ -3,13 +3,7 @@
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Data Barang Inventaris</h1>
-        <a href="<?= base_url() ?>barang/tambah" class="btn btn-sm btn-primary btn-icon-split">
-            <span class="text text-white">Tambah Data</span>
-            <span class="icon text-white-50">
-                <i class="fas fa-plus"></i>
-            </span>
-        </a>
+        <h1 class="h3 mb-0 text-gray-800">Data Pengajuan</h1>
     </div>
 
     <div class="col-lg-12 mb-4" id="container">
@@ -22,31 +16,26 @@
                         <thead>
                             <tr>
                                 <th width="1%">No</th>
-                                <th>Foto</th>
-                                <th>Nama Barang</th>
-                                <th>Posisi</th>
-                                <th>Kategori</th>
-                                <th>Tanggal Masuk</th>
+                                <th>Nama Tenaga Kesehatan</th>
+                                <th>Nama Mitra Bestari</th>
+                                <th>Tanggal Pengajuan</th>
                                 <th width="1%">Aksi</th>
                             </tr>
                         </thead>
                         <tbody style="cursor:pointer;" id="tbody"> 
-                            <?php $no=1; foreach ($barang as $b): ?>
+                            <?php $no=1; foreach ($pengajuan as $p): ?>
                             <tr>
-                                <td onclick="detail('<?= $b->id_barang ?>')"><?= $no++ ?>.</td>
-                                <td onclick="detail('<?= $b->id_barang ?>')"><img style="border-radius: 5px;"
-                                        src="assets/upload/barang/<?= $b->foto ?>" alt="" width="75px"></td>
-                                <td onclick="detail('<?= $b->id_barang ?>')"><?= $b->nama_barang ?></td>
-                                <td onclick="detail('<?= $b->id_barang ?>')"><?= $b->posisi ?></td>
-                                <td onclick="detail('<?= $b->id_barang ?>')"><?= $b->nama_kategori ?></td>
-                                <td onclick="detail('<?= $b->id_barang ?>')"><?= $b->tanggal_masuk ?></td>
+                                <td onclick="#"><?= $no++ ?>.</td>
+                                <td onclick="#"><?= $p->nama ?></td>
+                                <td onclick="#"><?= $p->nama_mitra ?></td>
+                                <td onclick="#"><?= $p->tanggal_pengajuan ?></td>
                                 <td>
                                     <center>
-                                        <a href="<?= base_url() ?>barang/ubah/<?= $b->id_barang ?>"
+                                        <a href="#"
                                             class="btn btn-circle btn-success btn-sm">
                                             <i class="fas fa-pen"></i>
                                         </a>
-                                        <a href="#" onclick="konfirmasi('<?= $b->id_barang ?>')"
+                                        <a href="#" onclick="#"
                                             class="btn btn-circle btn-danger btn-sm">
                                             <i class="fas fa-trash"></i>
                                         </a>
