1c62571f30c69833e0446d721d763174efffd5c0
Hizkia Syaukani <hizkiapro980@gmail.com>
2023-05-01 20:55:33 +0700

    Upload Berkas 2/3

--- assets/js/validasi/formbarang.js
+++ assets/js/validasi/formbarang.js
@@ -1,6 +1,3 @@
-
-
-
 function validasi(judul, status) {
     swal.fire({
         title: judul,
@@ -21,40 +18,39 @@ function fileIsValid(fileName) {
         case 'tiff':
         case 'gif':
         case 'tif':
+        case 'pdf':
 
             break;
         default:
             this.value = '';
-            isValid = false;
+            isValid = true;
     }
     return isValid;
 }
 
-function VerifyFileNameAndFileSize() {
-    var file = document.getElementById('GetFile').files[0];
+// function VerifyFileNameAndFileSize() {
+//     var file = document.getElementById('pasfoto').files[0];
+//     if (file != null) {
+//         var fileName = file.name;
+//         if (fileIsValid(fileName) == false) {
+//             validasi('Format bukan gambar!', 'warning');
+//             document.getElementById('pasfoto').value = null;
+//             return false;
+//         }
+//         var content;
+//         var size = file.size;
+//         if ((size != null) && ((size / (1024 * 1024)) > 3)) {
+//             validasi('Ukuran maximum 1024px', 'warning');
+//             document.getElementById('pasfoto').value = null;
+//             return false;
+//         }
 
+//         var ext = fileName.match(/\.([^\.]+)$/)[1];
+//         ext = ext.toLowerCase();
+//         $(".custom-file-label").addClass("selected").html(file.name);
+//         // document.getElementById('outputImg').src = window.URL.createObjectURL(file);
+//         return true;
 
-    if (file != null) {
-        var fileName = file.name;
-        if (fileIsValid(fileName) == false) {
-            validasi('Format bukan gambar!', 'warning');
-            document.getElementById('GetFile').value = null;
-            return false;
-        }
-        var content;
-        var size = file.size;
-        if ((size != null) && ((size / (1024 * 1024)) > 3)) {
-            validasi('Ukuran maximum 1024px', 'warning');
-            document.getElementById('GetFile').value = null;
-            return false;
-        }
-
-        var ext = fileName.match(/\.([^\.]+)$/)[1];
-        ext = ext.toLowerCase();
-        $(".custom-file-label").addClass("selected").html(file.name);
-        document.getElementById('outputImg').src = window.URL.createObjectURL(file);
-        return true;
-
-    } else
-        return false;
-}
+//     } else
+//         return false;
+// }
