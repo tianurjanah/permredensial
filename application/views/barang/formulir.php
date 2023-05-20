03a9d9bcd6b106e29407a6cd9c07da42200e6b57
Tia Nurjanah <tnurjanah85@gmail.com>
2023-05-20 19:21:36 +0700

    test

--- application/views/barang/formulir.php
+++ application/views/barang/formulir.php
@@ -228,7 +228,7 @@
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                </tbody>
+                                                </tbody>  
                                             </table>
                                         </div>
                                     </div>                                
