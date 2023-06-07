5a2c8bc8dda3725f7f3a0f8da94f9cb4f3c30fb7
Hizkia Syaukani <hizkiapro980@gmail.com>
2023-06-07 17:44:26 +0700

    dah selesai

--- application/controllers/jadwal_kredensial.php
+++ application/controllers/jadwal_kredensial.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Approve_pengajuan extends CI_Controller
+class jadwal_kredensial extends CI_Controller
 {
 
     public function __construct()
