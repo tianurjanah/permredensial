4b45580b1af9177e5d9d1afd49da59139b0a5df4
Hizkia Syaukani <hizkiapro980@gmail.com>
2023-06-07 12:15:40 +0700

    aku simpen di approve_pengajuan_model

--- application/models/approve_pengajuan_model.php
+++ application/models/approve_pengajuan_model.php
@@ -30,4 +30,15 @@ class approve_pengajuan_model extends ci_model
         return $query;
     }
 
+    public function getKompetensiData($id_pengajuan_index)
+    {
+        $this->db->select('kompetensi.bagian, kompetensi.kompetensi');
+        $this->db->from('kompetensi');
+        $this->db->join('pengajuan', 'pengajuan.id_kompetensi = kompetensi.id_kb', 'right');
+        $this->db->where('kompetensi.bagian', 'b1');
+        $this->db->where('pengajuan.id_pengajuan_index', $id_pengajuan_index);
+        $query = $this->db->get();
+        return $query->result();
+
+    }
 }
