9a32b8230c8a8853e7a8f48d7e8663579588859b
Hizkia Syaukani <hizkiapro980@gmail.com>
2023-06-11 06:55:12 +0700

    PDF

---
+++ application/views/pdf/rincian_kewenangan.php
@@ -0,0 +1,123 @@
+<table style="width: 100%; border-collapse: collapse; border: 1px solid rgb(0, 0, 0);">
+    <thead>
+        <tr>
+            <th style="width: 5.7692%; border: 1px solid rgb(0, 0, 0);">NO</th>
+            <th style="width: 79.4289%; border: 1px solid rgb(0, 0, 0);">RINCIAN KEWENANGAN KLINIS</th>
+            <th style="border: 1px solid rgb(0, 0, 0); width: 14.6106%;">KEWENANGAN DIBERIKAN (*)</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td style="width: 100%; border: 1px solid rgb(0, 0, 0);" colspan="3">
+                <div style="text-align: center;"><strong>MELAKUKAN PEMERIKSAAN TEKNIK RADIOGRAFI KONVENSIONAL TANPA
+                        KONTRAS</strong></div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 5.7692%; border: 1px solid rgb(0, 0, 0);">
+                <div data-empty="true" style="text-align: center;"><br></div>
+            </td>
+            <td style="width: 79.4289%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 14.6106%; border: 1px solid rgb(0, 0, 0);">
+                <div data-empty="true" style="text-align: center;"><br></div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 99.8834%; border: 1px solid rgb(0, 0, 0);" colspan="3">
+                <div style="text-align: center;"><strong>MELAKUKAN PEMERIKSAAN TEKNIK RADIOGRAFI KONVENSIONAL DENGAN
+                        KONTRAS</strong></div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 5.7692%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 79.4289%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 14.6106%; border: 1px solid rgb(0, 0, 0);"><br></td>
+        </tr>
+        <tr>
+            <td style="width: 99.8834%; border: 1px solid rgb(0, 0, 0);" colspan="3">
+                <div style="text-align: center;"><strong>MELAKUKAN PEMERIKSAAN TEKNIK CT SCAN TANPA KONTRAS</strong>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 5.7692%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 79.4289%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 14.6106%; border: 1px solid rgb(0, 0, 0);"><br></td>
+        </tr>
+        <tr>
+            <td style="width: 99.8834%; border: 1px solid rgb(0, 0, 0);" colspan="3">
+                <div style="text-align: center;"><strong>MELAKUKAN PEMERIKSAAN TEKNIK CT SCAN DENGAN KONTRAS</strong>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 5.7692%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 79.4289%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 14.6106%; border: 1px solid rgb(0, 0, 0);"><br></td>
+        </tr>
+        <tr>
+            <td style="width: 99.8834%; border: 1px solid rgb(0, 0, 0);" colspan="3">
+                <div style="text-align: center;"><strong>MELAKUKAN PEMERIKSAAN TEKNIK MRI TANPA KONTRAS</strong></div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 5.7692%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 79.4289%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 14.6106%; border: 1px solid rgb(0, 0, 0);"><br></td>
+        </tr>
+        <tr>
+            <td style="width: 99.8834%; border: 1px solid rgb(0, 0, 0);" colspan="3">
+                <div style="text-align: center;"><strong>MELAKUKAN PEMERIKSAAN TEKNIK MRI DENGAN KONTRAS</strong></div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 5.7692%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 79.4289%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 14.6106%; border: 1px solid rgb(0, 0, 0);"><br></td>
+        </tr>
+        <tr>
+            <td style="width: 99.8834%; border: 1px solid rgb(0, 0, 0);" colspan="3">
+                <div style="text-align: center;"><strong>MELAKUKAN PEMERIKSAAN TEKNIK RADIOGRAFI MENGGUNAKAN PERALATAN
+                        USG</strong></div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 5.7692%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 79.4289%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 14.6106%; border: 1px solid rgb(0, 0, 0);"><br></td>
+        </tr>
+        <tr>
+            <td style="width: 99.8834%; border: 1px solid rgb(0, 0, 0);" colspan="3">
+                <div style="text-align: center;"><strong>MELAKUKAN PEMERIKSAAN TEKNIK RADIOGRAFI KEDOKTERAN
+                        NUKLIR</strong></div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 5.7692%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 79.4289%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 14.6106%; border: 1px solid rgb(0, 0, 0);"><br></td>
+        </tr>
+        <tr>
+            <td style="width: 99.8834%; border: 1px solid rgb(0, 0, 0);" colspan="3">
+                <div style="text-align: center;"><strong>MELAKUKAN PEMERIKSAAN TEKNIK RADIOTERAPI DENGAN MODALITAS
+                        RADIOTERAPI EKSTERNAL DAN/ATAU INTERNAL</strong></div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 5.7692%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 79.4289%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 14.6106%; border: 1px solid rgb(0, 0, 0);"><br></td>
+        </tr>
+        <tr>
+            <td style="width: 99.8834%; border: 1px solid rgb(0, 0, 0);" colspan="3">
+                <div style="text-align: center;"><strong>MELAKUKAN QUALITY ASSURANCE/ QUALITY CONTROL BEKERJA SAMA
+                        DENGAN MITRA TERKAIT</strong></div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 5.7692%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 79.4289%; border: 1px solid rgb(0, 0, 0);"><br></td>
+            <td style="width: 14.6106%; border: 1px solid rgb(0, 0, 0);"><br></td>
+        </tr>
+    </tbody>
+</table>
