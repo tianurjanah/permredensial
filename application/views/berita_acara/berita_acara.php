6425e3f9213314fd0a0d5cd921ccdbb8123b9de0
Tia Nurjanah <tnurjanah85@gmail.com>
2023-06-10 18:30:02 +0700

    pdf berita acara

---
+++ application/views/berita_acara/berita_acara.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <style>
+    .line-title {
+        border: 0;
+        border-style: inset;
+        border-top: 1px solid #000;
+    }
+
+    table {
+        border-collapse: collapse;
+    }
+
+    .centered-image {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    </style>
+</head>
+
+<body>
+    <center>
+        <div>
+            <img src="<?= base_url('assets/img/logo.png') ?>" class="centered-image" alt="Logo" />
+        </div>
+
+    </center>
+
+
+
+
+    <!-- <table style="width: 100%;">
+		<tr>
+			<td align="center">
+				<span style="line-height: 1.6; font-weight: bold; font-size: 20;">
+					PT. EZZER KEMINDO MULIATAMA
+				</span>
+				<span style="line-height: 1.6; font-size: 10">
+					<br>
+					Kawasan Kopo Mas Regency Kav.99A. Kopo Bandung 40225 - Jawa Barat
+					<br>
+					Telp : +62 22 5430 | Fax : +62 22 5436 851 | Po. Box 123 Bks 17000
+					<br>
+					www.ezzer.co.id | www.ekmgroup.asia/ezzer | jkt@ezzer.co.id
+				</span>
+			</td>
+		</tr>
+	</table> -->
+    <!-- <br>
+	<hr class="line-title">
+	<br>
+	<br>
+	<br>
+	<h3 style="text-align : center;">LAPORAN BARANG INVENTARIS PT. EZZER KEMINDO MULIATAMA</h3>
+	<br>
+	<br>
+	<table border="1" style="width: 100%;">
+
+	</table>
+	<br><br>
+	<footer>
+		<p style="text-align : right; font-size: 12; margin-right:40px;">
+			Bandung, <?php echo $date; ?>
+		</p>
+		<br><br>
+		<br>
+		<p style="text-align : right; font-size: 12; margin-right:75px;">
+			<?php echo $user; ?>
+		</p>
+	</footer> -->
+</body>
+
+</html>
